dcf4c8a0ea804dfac2062a4be17b0a0329706631
jonathanhhoward <2773262@gmail.com>
2020-11-18 16:39:20 -0500

    Module 3-1

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-import { Database } from "../lib/types";
+import { Database, Booking, Listing, User } from "../lib/types";
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_CLSTR}.mongodb.net/main?retryWrites=true&w=majority`;
 
@@ -11,6 +11,8 @@ export async function connectDatabase(): Promise<Database> {
   const db = client.db("main");
 
   return {
-    listings: db.collection("test_listings"),
+    bookings: db.collection<Booking>("bookings"),
+    listings: db.collection<Listing>("listings"),
+    users: db.collection<User>("users"),
   };
 }
