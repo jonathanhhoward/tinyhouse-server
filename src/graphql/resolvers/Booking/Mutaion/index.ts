3191eab6f255973520ee31e84724bc723d0758e1
jonathanhhoward <2773262@gmail.com>
2021-01-19 16:55:12 -0500

    Module 13-1

---
+++ src/graphql/resolvers/Booking/Mutaion/index.ts
@@ -0,0 +1,3 @@
+export function createBooking(): string {
+  return "Mutation.createBooking";
+}
--- src/graphql/resolvers/Booking/index.ts
+++ src/graphql/resolvers/Booking/index.ts
@@ -1,7 +1,11 @@
 import { IResolvers } from "apollo-server-express";
+import { createBooking } from "./Mutaion";
 import { id, listing } from "./Booking";
 
 export const bookingResolvers: IResolvers = {
+  Mutation: {
+    createBooking,
+  },
   Booking: {
     id,
     listing,
